711c2d9101d4fd1813a1a579e88c6ff44b4e2452
ergo lance <brainispower21@gmail.com>
2022-04-08 11:53:42 +0300

    Add Project to folder.

---
+++ MangoRead/MangoRead/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MangoRead.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
