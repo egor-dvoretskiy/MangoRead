bb29f30b83fa46d9aa41b8465e5bbd1e5ea6d924
ergo lance <brainispower21@gmail.com>
2022-04-26 15:19:47 +0300

    Add ManageUserRole page.

---
+++ MangoRead/MangoRead.Domain/ViewModels/Roles/ManageUserRolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoRead.Domain.ViewModels.Roles
+{
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
