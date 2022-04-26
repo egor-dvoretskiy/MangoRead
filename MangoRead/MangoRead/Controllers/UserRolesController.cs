4ba23381cfb3cea0d602ed98711fa3b31bc875d7
ergo lance <brainispower21@gmail.com>
2022-04-26 10:22:15 +0300

    Add RoleManager view, UserRoles view.

---
+++ MangoRead/MangoRead/Controllers/UserRolesController.cs
@@ -0,0 +1,50 @@
+﻿using MangoRead.Domain.Models.Account;
+using MangoRead.Domain.ViewModels.Roles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoRead.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.UserName = user.UserName;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.Email = user.Email;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+
+            return View(userRolesViewModel);
+        }
+
+        private async Task<List<string>> GetUserRoles(ApplicationUser user)
+        {
+            return new List<string> (await _userManager.GetRolesAsync(user));
+        }
+    }
+}
